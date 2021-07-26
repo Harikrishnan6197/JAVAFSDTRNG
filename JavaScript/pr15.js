ec67733cc3082ffd118e9c03d74aaffb0883d3e1
Harikrishnan6197 <74889097+Harikrishnan6197@users.noreply.github.com>
2021-07-26 12:37:40 +0530

    Add files via upload

---
+++ JavaScript/pr15.js
@@ -0,0 +1,6 @@
+
+const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+
+
+document.getElementById("demo").innerHTML =
+person.firstName + " is " + person.age + " years old.";
---
+++ JavaScript/pr37.js
@@ -0,0 +1,12 @@
+
+function Person(first, last, age, eye) {
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = eye;
+}
+
+const myFather = new Person("John", "Doe", 50, "blue");
+
+document.getElementById("demo").innerHTML =
+"My father is " + myFather.age + "."; 
