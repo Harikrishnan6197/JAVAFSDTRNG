9049907d8634e4e82c5cd098a173859ee43b149f
Harikrishnan6197 <74889097+Harikrishnan6197@users.noreply.github.com>
2021-08-23 09:56:22 +0530

    Add files via upload

---
+++ Employeeproject/SpringController.java
@@ -0,0 +1,56 @@
+package com.ust.springboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SpringController {
+	@Autowired
+	EmpService empservice;
+	@RequestMapping("/")
+public String index() {
+		return "index.html";
+		
+	}
+	@GetMapping("/home")
+	public String viewhomepage(Model model) {
+		model.addAttribute("listEmployees" ,empservice.getemployees());
+		return "home";
+		
+	}
+	
+	@GetMapping("/newEmpform")
+	public String showemployeeform(Model model) {
+		Emp employee=new Emp();
+		model.addAttribute("employee",employee);
+		return "newEmpform";
+	}
+	
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute ("emmployee") Emp employee) {
+		empservice.saveEmployee(employee);
+		return "redirect:/";
+	}
+	
+	@GetMapping("/showFormForUpdate/{id}") 
+	public String showForm(@PathVariable (value="id")long id,Model model) {
+		Emp employee=empservice.getEmployeeById(id);
+		model.addAttribute("employee",employee);
+		return "updateform";
+	}
+	
+	@GetMapping("/deleteEmployee/{id}") 
+	public String showForm(@PathVariable (value="id")long id) {
+	 this.empservice.deleteEmployeeById(id);
+	 return "redirect:/";
+	}
+	
+	
+	
+}
